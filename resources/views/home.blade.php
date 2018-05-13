839714b6aacdb01f879c3d00732df3e280e9a7f0
Unknown <Geminigirl124@gmail.com>
2018-05-13 23:14:26 +0700

    r2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,19 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
-</head>
+    <title> Makanan Nusantara </title>
+    <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Mr+Dafoe" rel="stylesheet">
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    </head>
+
 <body>
 
+    <section class="background" >
+     
+    </section>
+    
+  
 </body>
 </html>
