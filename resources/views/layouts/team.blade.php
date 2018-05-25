b21f79dd11b0502ac6d822c693fa7d9bd91ccc0c
Rifka <Geminigirl124@gmail.com>
2018-05-26 00:36:53 +0700

    U-DIVA

--- resources/views/layouts/team.blade.php
+++ resources/views/layouts/team.blade.php
@@ -2,58 +2,55 @@
 		<div class="container">
 			<div class="row animate-box">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-					<h2>Fitness Expert</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+					<h2>TEAM U-DIVA</h2>
+					{{-- <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p> --}}
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-4 col-sm-4 animate-box">
 					<div class="trainer">
-						<a href="#"><img class="img-responsive" src="img/helpyow.jpg" alt="trainer"></a>
+						<a href="#"><img class="img-responsive" src="img/helpyow.png" alt="trainer"></a>
 						<div class="title">
-							<h3><a href="#">Angel Adams</a></h3>
-							<span>Dance Expert</span>
+							<h3><a href="#">Navinda Meutia</a></h3>
+							<span>5116100015</span>
 						</div>
 						<div class="desc text-center">
 							<ul class="fh5co-social-icons">
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-linkedin"></i></a></li>
-								<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								<li><a href="https://twitter.com/NmMutia?lang=en"><i class="icon-twitter"></i></a></li>
+								<li><a href="https://www.facebook.com/navinda.meutia"><i class="icon-facebook"></i></a></li>
+								
 							</ul>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-4 animate-box">
 					<div class="trainer">
-						<a href="#"><img class="img-responsive" src="img/helpyow.jpg.jpg" alt="trainer"></a>
+						<a href="#"><img class="img-responsive" src="img/helpyow.png" alt="trainer"></a>
 						<div class="title">
-							<h3><a href="#">Arnold Smith</a></h3>
-							<span>Body Building Expert</span>
+							<h3><a href="#">Fadilla Sukma A</a></h3>
+							<span>5116100024</span>
 						</div>
 						<div class="desc text-center">
 							<ul class="fh5co-social-icons">
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-linkedin"></i></a></li>
-								<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								<li><a href="https://twitter.com/fadillasa?lang=en"><i class="icon-twitter"></i></a></li>
+								<li><a href="https://www.facebook.com/fadillasukmaalfiani"><i class="icon-facebook"></i></a></li>
+
 							</ul>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-4 animate-box">
 					<div class="trainer">
-						<a href="#"><img class="img-responsive" src="images/img/helpyow.jpg.jpg" alt="trainer"></a>
+						<a href="#"><img class="img-responsive" src="img/helpyow.png" alt="trainer"></a>
 						<div class="title">
-							<h3><a href="#">Rachel Seely</a></h3>
-							<span>Yoga Expert</span>
+							<h3><a href="#">Rifka Annisa</a></h3>
+							<span>5116100036</span>
 						</div>
 						<div class="desc text-center">
 							<ul class="fh5co-social-icons">
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-linkedin"></i></a></li>
-								<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								<li><a href="https://twitter.com/Hitori124?lang=en"><i class="icon-twitter"></i></a></li>
+								<li><a href="https://www.facebook.com/Yatogami.Risa"><i class="icon-facebook"></i></a></li>
+						
 							</ul>
 						</div>
 					</div>
