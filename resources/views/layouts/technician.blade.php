e13f519f4a6574550e505d3f687763a1ef30bc8e
Unknown <nm.mutia02@gmail.com>
2018-05-26 00:58:53 +0700

    teknisi

---
+++ resources/views/layouts/technician.blade.php
@@ -0,0 +1,94 @@
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url('/img/teknisi.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>TECHNICIAN</h1>
+							<h2>Helpyow membantu anda untuk mencari, menemukan, menyalurkan pekerja dalam bidang teknik: Teknisi Elektronika, Teknisi Mekanika, <em>Handyman</em></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<section>
+	<div id="fh5co-gallery">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>TECHNICIAN</h2>
+					<p>Cari pekerja untuk meringankan pekerjaan rumah tangga anda hanya di Helpyow.</p>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row row-bottom-padded-md">
+				<div class="col-md-12">
+					<ul id="fh5co-portfolio-list">
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('/img/gallery-1.jpg'); ">
+							<a href="#">
+								<div class="case-studies-summary">
+									<span>Illustration</span>
+									<h2>Elektronika</h2>
+								</div>
+							</a>
+						</li>
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('/img/gallery-2.jpg'); ">
+							<a href="#">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>Mekanika</h2>
+								</div>
+							</a>
+						</li>
+
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('/img/gallery-3.jpg'); ">
+							<a href="#">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>Elektronika</h2>
+								</div>
+							</a>
+						</li>
+
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('/img/gallery-4.jpg'); "> 
+							<a href="#">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>Handyman</h2>
+								</div>
+							</a>
+						</li>
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('/img/gallery-3.jpg'); ">
+							<a href="#">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>Elektronika</h2>
+								</div>
+							</a>
+						</li>
+						<li class="two-third animate-box" data-animate-effect="fadeIn" style="background-image: url('/img/gallery-5.jpg'); ">
+							<a href="#">
+								<div class="case-studies-summary">
+									<span>Illustration</span>
+									<h2>Handyman</h2>
+								</div>
+							</a>
+						</li>
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('/img/gallery-3.jpg'); ">
+							<a href="#">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>Handyman</h2>
+								</div>
+							</a>
+						</li>
+					</ul>		
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
